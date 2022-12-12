52b8fbde785ac7de01bf86b766b331da4868d208
fakincow <zabaroth@gmail.com>
2022-12-13 00:48:20 +0200

    basic css select

--- angular-test-kramerav/src/app/components/card/card.component.ts
+++ angular-test-kramerav/src/app/components/card/card.component.ts
@@ -12,5 +12,15 @@ export class CardComponent {
   @Input() NativeResolution:string="device.NativeResolution"
   @Input() Size:number=0
   @Input() status:number =0
-
+  public selected:boolean = false;
+  public cardClass:string = 'default'
+  onClickEvent(event: any) {
+    this.selected = !this.selected;
+    console.log('selected',this.selected);
+   if(this.selected){
+    this.cardClass = 'selected'
+   }else{
+    this.cardClass = 'default'
+   }
+  }
 }
